16c638ff7b9616c72fc7b9d71d5f81b932558607
Sergey <bob3r@yahoo.com>
2020-04-21 17:38:43 +0700

    Add multithreading and timeout variable
    If you request pages too frequently, avito will require captcha.

--- parsing.py
+++ parsing.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from threading import Thread
 import time
 import csv
 
 URL = 'https://www.avito.ru/'
-CITY = 'novosibirsk'
-Q = 'logitech g pro'
+CITY = 'rossiya'
+Q = 'razer'
 HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36 OPR/67.0.3575.105',
 'accept':'*/*'}
+TO = 1
 
 def get_html(q, city = 'rossiya', page = 1):
 	options = webdriver.ChromeOptions()
@@ -15,7 +17,7 @@ def get_html(q, city = 'rossiya', page = 1):
 	browser = webdriver.Chrome('chromedriver.exe', options=options)
 	link = "{0}{1}?cd=1&q={2}&p={3}".format(URL, city, q.replace(' ', '+'), page)
 	browser.get(link)
-	time.sleep(1)
+	time.sleep(TO)
 	r = browser.page_source
 	return r
 
@@ -35,7 +37,7 @@ def get_content(html):
 				location = 'Не удалось обнаружить'
 		try:
 			goods.append({
-			'name':	i.find('a', class_='snippet-link').get_text(strip = True), #
+			'name':	i.find('a', class_='snippet-link').get_text(strip = True), 
 			'price': i.find('span', class_='snippet-price').get_text(strip = True).replace('\n', ''),
 			'location': location,
 			'date':	i.find('div', class_='snippet-date-info').get('data-tooltip'),
@@ -61,6 +63,43 @@ def save_data(goods, path):
 		writer.writerow(['Название', 'Цена', 'Адрес', 'Дата публикации', 'Ссылка'])
 		for i in goods:
 			writer.writerow([i['name'], i['price'], i['location'], i['date'], i['link']])
+
+
+class parsingThread(Thread):
+	def __init__(self, page):
+		Thread.__init__(self)
+		self.page = page
+		self.goods = []
+	def run(self):
+		print("Парсинг страницы номер {} начат".format(self.page))
+		self.html = get_html(Q, city = CITY, page = self.page)
+		self.goods.extend(get_content(self.html))
+		print("Парсинг страницы номер {} завершен".format(self.page))
+
+
+def parseWithThread():
+	html = get_html(Q, city = CITY)
+	x = get_max_page(html)
+	print("Найдено страниц: {}".format(x))
+
+	thread_list = []
+	for i in range(1, x+1):
+		thread = parsingThread(i)
+		thread.start()
+		thread_list.append(thread)
+
+	flag = True
+	while flag:
+		for i in range(0, x):
+			flag = flag and thread_list[i].is_alive()
+
+	goods = []
+	for i in range(0, x):
+		goods.extend(thread_list[i].goods)
+	save_data(goods, "{}.csv".format(Q))
+
+
+
 def parse():
 	html = get_html(Q, city = CITY)
 	x = get_max_page(html)
@@ -72,6 +111,6 @@ def parse():
 	save_data(goods, "{}.csv".format(Q))
 
 def main():
-	parse()
+	parseWithThread()
 if __name__ == '__main__':
 	main()
